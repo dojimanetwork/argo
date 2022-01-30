6652beabbb0af7b36fdfba6ee4088061cfd4ed1a
bhaagiKenpachi <bhagath.reddy@dojima.network>
2022-01-30 12:53:18 +0530

    added pst contract addresses.

--- utils/pst.go
+++ utils/pst.go
@@ -2,6 +2,18 @@ package utils
 
 import "github.com/dojimanetwork/argo/types"
 
+
+const (
+	ArDrive = "-8A6RexFkpfWwuyVO98wzSFZh0d6VJuI-buTJvlwOJQ"
+	Verto = "usjm4PCxUd5mtaon7zc97-dt-3qf67yPyqgzLnLqk5A"
+	ArVerify = "f6lW-sKxsc340p8eBBL2i_fnmSI_fRSFmkqvzqyUsRs"
+)
+
+var (
+ supportedPstContracts = [3]string{ArDrive, Verto, ArVerify}
+)
+
+
 func PstTransferTags(contractId string, target string, qty int64) ([]types.Tag, error) {
 	input := types.Input{
 		"function": "transfer",
@@ -21,4 +33,9 @@ func PstTransferTags(contractId string, target string, qty int64) ([]types.Tag, 
 		{Name: "Input", Value: inputStr},
 	}
 	return pstTags, nil
+}
+
+
+func GetSupportedPstContracts() []string {
+	return supportedPstContracts[:]
 }
